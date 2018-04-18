41a3b9ebe79922cf11e1fc5cd77559518065b66e
Useless-coderrr <37732454+Useless-coderrr@users.noreply.github.com>
2018-04-18 21:54:20 +0300

    some changes.

--- file.js
+++ file.js
@@ -1,6 +1,13 @@
 function submitted(){
  var Name=document.getElementById('username');
  var Age=document.getElementById('Age');
+ var Lang=document.getElementById('lang');
+ var Sex=document.getElementById('Sex');
+ var Job=document.getElementById('Job');
+ var App=document.getElementById('app');
+ var Code=document.getElementById('code');
+ var Desc=document.getElementById('desc');
+ 
  var config = {
     apiKey: "AIzaSyBlsQAcsXSsaXTIVkRxHwmwZ0j78O3FPwM",
     authDomain: "project-forum-ef6a3.firebaseapp.com",
@@ -14,12 +21,16 @@ function submitted(){
   var ref=database.ref('answers');
   var answers={
   	name: Name.value,
-  	Age: Age.value
+  	Age: Age.value,
+    Gender: Sex.value,
+    Job: Job.value,
+    Lang: Lang.value,
+    code: Code.value,
+    App: App.value,
+    Desc: Desc.value
   }
   console.log(answers);
- window.alert("your username is "+Name.value);
+ window.alert("Thank you for contributing "+Name.value);
  ref.push(answers);
-
-
 
 }
